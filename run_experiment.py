45d9f64d130aef785170c4b7683b478ca125f125
Luis Vivas <luis.alejandro.vivas@estudiantat.upc.edu>
2025-09-30 19:24:57 +0200

    Updated train and evaluate routine

--- run_experiment.py
+++ run_experiment.py
@@ -6,25 +6,9 @@ from src.data.return_sub_graph import get_subgraph_first_n_nodes
 import os
 
 def main(args):
-    if args.n_nodes == -1:
-        data = torch.load(args.processed_graph_path, weights_only=False)
-    else:
-        data = torch.load(args.processed_graph_path, weights_only=False)
-        data = get_subgraph_first_n_nodes(data, args.n_nodes)
+    print(f"Start training")
+    val_f1, val_auc, _, _ = train_and_evaluate(args.model, args.data, test_data = args.test_data ,print_flag = args.print_flag, log_file=args.log_file)
     
-    print(data)
-
-    # 2. Initialize model based on arguments
-    #if args.model == 'gcn':
-    #    model = GCN(in_channels=data.x.shape[1], 
-    #                        hidden_channels=args.hidden_channels, 
-    #                        out_channels=1)
-
-    # 3. Train and evaluate
-    #print(f"Training model...")
-    #val_f1, val_auc = train_and_evaluate(model, data)
-    #print(f'val_f1 = {val_f1} \n val_auc = {val_auc}')
-    # Here you would save the results, model, plots etc. to a unique directory.
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Run GNN Experiment')
@@ -32,11 +16,27 @@ if __name__ == '__main__':
     # Arguments for data, model, training etc.
     parser.add_argument('graph', type=str, help = 'Name of the file that stores the PyG Data object')
     parser.add_argument('--n_nodes', type=int, default = 50, help = 'Number of nodes to return from the complete graph. -1 to use the complete graph')
-    # parser.add_argument('--model', type=str, default='gcn', help='GNN model to use (gcn, gat, etc.)')
     parser.add_argument('--data_path', type=str, default='data', help='Path to the data directory')
-        
+    parser.add_argument('--print_flag', type=bool, default='False', help='')
+    parser.add_argument('--test_data', type=bool, default='False', help='')
+    parser.add_argument('--log_file_name', type=str, default='None', help='Name of the file to save results.')
     args = parser.parse_args()
-    args.processed_graph_path = f'{args.data_path}/3_processed/rel-amazon/tasks/user-churn/common_purchased_categories_by_string/{args.graph}'
+
     args.hidden_channels = 128
+    args.log_file = f'results/rel-amazon/tasks/user-churn/common_purchased_categories_by_string/{args.log_file_name}'
+    processed_graph_path = f'{args.data_path}/3_processed/rel-amazon/tasks/user-churn/common_purchased_categories_by_string/{args.graph}'
+
+    if args.n_nodes == -1:
+        args.data = torch.load(processed_graph_path, weights_only=False)
+    else:
+        data = torch.load(processed_graph_path, weights_only=False)
+        args.data = get_subgraph_first_n_nodes(data, args.n_nodes)
+
+    # If the degree one-hot-encoded vector is an integer, the GCNConv Layer raises an error
+    args.data.x = args.data.x.float()
+
+    args.model = GCN(in_channels=args.data.x.shape[1], 
+                        hidden_channels=args.hidden_channels, 
+                        out_channels=1)
 
     main(args)
--- src/models/train_evaluate.py
+++ src/models/train_evaluate.py
@@ -1,5 +1,7 @@
 import torch
 from sklearn.metrics import f1_score, roc_auc_score
+import os
+from src.graph_construction.build_graph import return_density
 
 def train_loop(model, optimizer, criterion, data, masks):
     model.train()              # Set model to training mode
@@ -24,8 +26,20 @@ def test(mask, model, data):
         auroc = roc_auc_score(true_np, probs_np)  # Use probabilities for AUC
     return acc, f1, auroc, true_np, pred_np
 
-def train_and_evaluate(model, data, early_stop_patience = 15, test_data = False):
+def train_and_evaluate(model, data, early_stop_patience = 15, test_data = False, print_flag = False, log_file=None):
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    
+    log_f = None
+    if log_file and print_flag:
+        os.makedirs(os.path.dirname(log_file), exist_ok=True)
+        log_f = open(log_file, 'w')
+
+    def custom_print(message):
+        if print_flag:
+            print(message)
+        if log_f:
+            log_f.write(str(message) + '\n')
+
     masks = data.masks
     data = data.to(device)
     optimizer = torch.optim.Adam(model.parameters(), weight_decay=0.001)
@@ -35,19 +49,38 @@ def train_and_evaluate(model, data, early_stop_patience = 15, test_data = False)
     epochs_no_improve = 0
     best_model_state = model.state_dict()
 
+
+
+    custom_print("*" * 92)
+    custom_print('Data object: ')
+    custom_print(f'{data}')
+    density = return_density(data.num_nodes, (len(data.edge_index[0])/2))
+    custom_print(f'Graph density: {density:.4f}%')
+
     for epoch in range(1, 101):
         train_loss = train_loop(model, optimizer, criterion, data, masks)
         val_acc, val_f1, val_auc, true_labels, pred_labels = test(masks['val_mask'], model, data)
 
+        if print_flag:
+            custom_print("*" * 92)
+            custom_print(f'Epoch: {epoch:03d} | Loss: {train_loss:.4f}, Val Acc: {val_acc:.4f}, Val F1: {val_f1:.4f}, Val AUROC: {val_auc:.4f}')
+
         if val_auc > best_val_auc:
+            if print_flag:
+                custom_print(f'\tEpoch: {epoch:03d} | Validation  improved from {best_val_auc:.2f} to {val_auc:.2f} in epoch {epoch:03d}')
             best_val_auc = val_auc
             best_model_state = model.state_dict()
             epochs_no_improve = 0
         else:
             epochs_no_improve += 1
+            if print_flag:
+                custom_print(f'\tEpoch: {epoch:03d} | No improvement for {epochs_no_improve}/{early_stop_patience} epochs')
 
         # Check early stopping condition
         if epochs_no_improve >= early_stop_patience:
+            if print_flag:
+                custom_print(f"Early stopping triggered after {epoch} epochs!")
+                custom_print("*" * 92)
             break
 
     # Load the best model
@@ -55,9 +88,18 @@ def train_and_evaluate(model, data, early_stop_patience = 15, test_data = False)
 
     if test_data:
         test_acc, test_f1, test_auc, true_labels, pred_labels = test(masks['test_mask'], model, data)
-        #return test_f1, test_auc, true_labels, pred_labels
-        return test_f1, test_auc
+        custom_print(f'test_f1 = {test_f1:.4f} \n test_auc = {test_auc:.4f}')
+        if log_f:
+            log_f.close()
+
+        return test_f1, test_auc, true_labels, pred_labels
+        
     else:
         val_acc, val_f1, val_auc, true_labels, pred_labels = test(masks['val_mask'], model, data)
-        #return val_f1, val_auc, true_labels, pred_labels
-        return val_f1, val_auc
+        custom_print(f'val_f1 = {val_f1:.4f} \n val_auc = {val_auc:.4f}')
+        if log_f:
+            log_f.close()
+
+        return val_f1, val_auc, true_labels, pred_labels
+
+        
