1f506962a0a3ce5deb5d106169be3f2e96ec2bdf
Luis Vivas <luis.alejandro.vivas@estudiantat.upc.edu>
2025-09-30 13:42:47 +0200

    Add data/ folder

--- run_experiment.py
+++ run_experiment.py
@@ -2,6 +2,7 @@ import argparse
 import torch
 from src.models.gcn import GCN 
 from src.models.train_evaluate import train_and_evaluate
+from data import create_partitions
 import os
 
 def main(args):   
@@ -23,11 +24,14 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Run GNN Experiment')
     
     # Arguments for data, model, training etc.
+    parser.add_argument('graph', type=str, help = 'Name of the file that stores the PyG Data object')
     parser.add_argument('--model', type=str, default='gcn', help='GNN model to use (gcn, gat, etc.)')
-    parser.add_argument('--data_path', type=str, default='data', help='Dataset to use')
+    parser.add_argument('--data_path', type=str, default='data', help='Path to the data directory')
         
     args = parser.parse_args()
-    args.processed_data_path = f'{args.data_path}/3_processed/rel-amazon/tasks/user-churn/data_thres_4.pt'
+    args.processed_data_path = f'{args.data_path}/3_processed/rel-amazon/tasks/user-churn/common_purchased_categories_by_string/{args.graph}'
     args.hidden_channels = 128
 
     main(args)
+
+    
